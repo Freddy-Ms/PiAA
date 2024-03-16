431af06a6af443229d2437912b6075a7c8a1a1e9
Freddy-Ms <maciejskrzypa2705@gmail.com>
2024-03-16 20:39:14 +0100

    HeapUp

--- projekt1/HQueue.cpp
+++ projekt1/HQueue.cpp
@@ -1,24 +1,37 @@
 #include "HQueue.hpp"
 #include <iostream>
+#include <random>
 using namespace std;
 
 PriorityQueue::PriorityQueue(int size) : size(size) , actualsize(0){
     tab = new Packet[size];
 };
 PriorityQueue::~PriorityQueue(){ delete[] tab;};
-void PriorityQueue::repairUp(int i){
-
-};
-void PriorityQueue::repairDown(int i){
-
-};
 void PriorityQueue::add(Packet packet)
 {
-
+    tab[actualsize] = packet;
+    actualsize++;
 };
-void PriorityQueue::get_heap(){
-
+void PriorityQueue::generate_mssg(int n){
+    if(size >= n)
+    {
+    for(int i = 0; i < n; i++){
+        tab[actualsize].data = rand() % 100000;
+        tab[actualsize].priority = rand() % 100 + 1;
+        actualsize++;
+    };
+    }else{
+        resize();
+        generate_mssg(n);
+    };
 };
+void PriorityQueue::resize(){
+    int newsize = 2 * size;
+    Packet *nTab = new Packet[newsize];
+    delete[] tab;
+    tab = nTab;
+    size = newsize;    
+}
 bool PriorityQueue::isempty(){
     return actualsize == 0;
 };
@@ -29,7 +42,19 @@ void PriorityQueue::show(){
 };
 void PriorityQueue::make_heap(){
 
-};
+    for(int i = 1;i<actualsize;i++)
+    {
+        int child = i;
+        int parent = (child - 1) / 2;
+        int value = tab[child].priority;
+        while((child > 0) && (tab[parent].priority < value))
+        {
+            tab[child].priority = tab[parent].priority;
+            child = parent; parent = (child - 1) / 2;
+        }
+        tab[child].priority = value;
+    }
+};    
 void PriorityQueue::heap_sort(){
 
 };
--- projekt1/HQueue.hpp
+++ projekt1/HQueue.hpp
@@ -10,15 +10,14 @@ class PriorityQueue{
         Packet * tab;
         int size;
         int actualsize;
-        void repairUp(int i);
-        void repairDown(int i);
     public:
     PriorityQueue(int size);
     ~PriorityQueue();                        // Dwie formy, jedna po kolei dodajemy pakiet i "naprawiamy kopiec" albo od razu
     void add(Packet packet);
-    void get_heap();
+    void generate_mssg(int n);
     bool isempty();
     void show();                        //Pokazuje tylko priority, to jest dla nas najwazniejsze
     void make_heap();
     void heap_sort();
+    void resize();
 };                                           // generujemy n pakietów i tworzymy kopiec a następnie go całego ściągamy
