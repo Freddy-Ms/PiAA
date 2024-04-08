7eb0a10525e0a36dd9dc4cc70502ca61051bded4
Freddy-Ms <maciejskrzypa2705@gmail.com>
2024-04-08 15:56:23 +0200

    Driver
    added little driver

--- Mikroprojekt1/main.cpp
+++ Mikroprojekt1/main.cpp
@@ -4,17 +4,26 @@
 using namespace std;
 
 
-const int n = 10000000;
-int main()
-{
-    PriorityQueue Q(n);
-    Q.generate_mssg_sorted(n);
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    PriorityQueue* Q = new PriorityQueue(n); 
+    cout << "The elements are: \n";
+    cout << "1. Random\n2. Sorted\n";
+    int choice;
+    cin >> choice;
+    if(choice == 2)
+        Q->generate_mssg_sorted(n);
+    else
+        Q->generate_mssg(n);
     auto start = chrono::high_resolution_clock::now();
-    Q.make_heap();
-    Q.heap_sort();
+    Q->make_heap();
+    Q->heap_sort();
     auto end = chrono::high_resolution_clock::now();
     chrono::duration<double> elapsed = end - start;
     double time_taken = elapsed.count();
     cout << "Time taken by function: " << time_taken << " seconds" << endl;
-    Q.show();
+    delete Q; 
+    return 0;
 }
